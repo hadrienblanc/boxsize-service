fd9fb5340f9b4bda6d52460140c7dd4946061ee8
Hadrien Blanc <blanc.hadrien@gmail.com>
2018-06-11 22:35:19 +0200

    add flag pkg for the port number

--- main.go
+++ main.go
@@ -4,33 +4,23 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"strconv"
+  "flag"
 )
 
 const (
 	DefaultPort = 3000
 )
 
-func ArgsToPortNumber(args []string) int {
-	if len(args) > 1 {
-		port, err := strconv.ParseInt(args[1], 10, 64)
-		if err == nil {
-			return int(port)
-		}
-	}
-
-	return DefaultPort
-}
-
 func main() {
+	var port = flag.Int("port", DefaultPort, "Specify the port number")
+	flag.Parse()
+
 	server := http.NewServeMux()
 	server.HandleFunc("/", BoxSizeHandler)
 
-	port := ArgsToPortNumber(os.Args)
-	fmt.Printf("BoxSize Server will run on port %d\n", port)
+	fmt.Printf("BoxSize Server will run on port %d\n", *port)
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), server)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", *port), server)
 
 	if err != nil {
 		log.Fatal("cannot start server", err)
